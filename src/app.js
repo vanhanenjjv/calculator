08c508831cbc7b2ea2fcd41de63c16f7e75e0353
Janne Vanhanen <janne.vanhanen@outlook.com>
2022-04-03 16:18:56 +0300

    Implement divide function (#6)
    * Implement divide function
    
    * Fix imports/exports

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 
+import divide from './routes/divide.js'
 import subtractRouter from './routes/subtract.js'
 
 const PORT = 8080
@@ -13,6 +14,8 @@ server.use('/subtract', subtractRouter)
 server.get('/', (req, res) => {
   res.send('Hello, world!')
 })
+
+server.use('/divide', divide)
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
