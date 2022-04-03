e43bf9dd02e40801aa0310ef8efcdd63e825ec89
tominkki <57665651+tominkki@users.noreply.github.com>
2022-04-03 13:34:36 +0000

    FEAT: Implement add router (#9)
    :gem:

--- src/app.js
+++ src/app.js
@@ -1,26 +1,26 @@
-import express from 'express'
+import express from "express";
 
+import { addRouter } from "./routes/add/index.js";
 import multiply from "./routes/multiply.js";
 import divide from './routes/divide.js'
 import subtractRouter from './routes/subtract.js'
 
+const PORT = 8080;
 
-const PORT = 8080
+const server = express();
 
-const server = express()
-
-server.use(express.json())
+server.use(express.json());
 
 server.use('/subtract', subtractRouter)
 
-server.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+server.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
 
-
+server.use("/add", addRouter);
 server.use("/multiply", multiply);
 server.use('/divide', divide)
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
