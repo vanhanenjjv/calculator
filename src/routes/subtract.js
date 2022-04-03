8adb996e4390a8f24c103fc873ad55319f2a52ba
aarotoiv <aaro.toivonen@gmail.com>
2022-04-03 16:14:26 +0300

    Add NL and fix spacing

--- src/routes/subtract.js
+++ src/routes/subtract.js
@@ -6,7 +6,7 @@ router.post('/', (req, res) => {
   const { minuend, subtrahend } = req.body;
   const result = minuend - subtrahend
 
-  return res.json({result})
+  return res.json({ result })
 })
 
 export default router
